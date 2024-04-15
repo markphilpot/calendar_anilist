d1e6e86c9a21458ba1e6c4f03c290b325864a2fb
Mark Philpot <mark.philpot@gmail.com>
2024-04-15 02:06:59 +0000

    tweak title font size

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -72,9 +72,9 @@ const Card = (props: Props) => {
         </div>
       )}
       {title && (
-        <div className="flex h-full flex-grow flex-col overflow-hidden gap-4">
+        <div className="flex h-full flex-grow flex-col overflow-hidden gap-4 pt-1">
           <div
-            className={classNames('ml-2 max-h-[114px] overflow-hidden text-ellipsis font-lato text-sm font-bold', {
+            className={classNames('ml-2 max-h-[114px] overflow-hidden text-ellipsis font-lato text-xs font-bold line-clamp-3', {
               'ml-2 text-base': scaleCard,
             })}
           >
