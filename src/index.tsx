bd2b5a2f0114162aecc992adf1ce0811e4c2654c
Mark Philpot <mark.philpot@gmail.com>
2026-01-17 09:23:09 -0800

    Migrate from CRA to vite
    Upgrade tailwind to v4 as well

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import './tailwind.css';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { ApolloClient, ApolloProvider, InMemoryCache, NormalizedCacheObject } from '@apollo/client';
 import { BrowserRouter } from 'react-router-dom';
 import { LocalStorageWrapper, CachePersistor } from 'apollo3-cache-persist';
@@ -58,8 +57,3 @@ const Index = () => {
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(<Index />);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
