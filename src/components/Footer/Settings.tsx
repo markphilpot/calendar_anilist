92302ece41060720a51ffeeba29cecd22db4c8f8
Mark Philpot <mark.philpot@gmail.com>
2026-01-17 11:48:03 -0800

    Clean up settings

--- src/components/Footer/Settings.tsx
+++ src/components/Footer/Settings.tsx
@@ -4,6 +4,8 @@ import useSettings from '../../hooks/useSettings';
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input.tsx';
 import { Button } from '@/components/ui/button.tsx';
+import { Checkbox } from '@/components/ui/checkbox.tsx';
+import { Label } from '@/components/ui/label.tsx';
 
 const Settings = () => {
   const [open, setOpen] = useState(false);
@@ -66,15 +68,12 @@ const Settings = () => {
 
           <div className="mt-2 text-left">
             <div className={'mb-4'}>
-              <label htmlFor="anilist-username" className="block text-sm font-medium">
-                Anilist Username
-              </label>
+              <Label htmlFor="anilist-username">Anilist Username</Label>
               <div className="mt-1">
                 <Input
                   type="text"
                   name="anilist-username"
                   id="anilist-username"
-                  className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                   placeholder=""
                   value={username}
                   onChange={(e) => setUsername(e.target.value)}
@@ -83,78 +82,64 @@ const Settings = () => {
             </div>
 
             <div className="relative mb-4 flex items-start">
-              <div className="flex h-5 items-center">
-                <input
+              <div className="flex items-center">
+                <Checkbox
                   id="start-sunday"
                   aria-describedby="start-sunday-description"
                   name="start-sunday"
-                  type="checkbox"
-                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
                   checked={startsSunday}
-                  onChange={() => setStartsSunday(!startsSunday)}
+                  onCheckedChange={() => {
+                    setStartsSunday(!startsSunday);
+                  }}
                 />
               </div>
-              <div className="ml-3 text-sm">
-                <label htmlFor="start-sunday" className="font-medium">
-                  Week Starts on Sunday
-                </label>
+              <div className="ml-3">
+                <Label htmlFor="start-sunday">Week Starts on Sunday</Label>
               </div>
             </div>
 
             <div className="relative mb-4 flex items-start">
-              <div className="flex h-5 items-center">
-                <input
+              <div className="flex items-center">
+                <Checkbox
                   id="show-empty-days"
                   aria-describedby="show-empty-days-description"
                   name="show-empty-days"
-                  type="checkbox"
-                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
                   checked={emptyDays}
-                  onChange={() => setEmptyDays(!emptyDays)}
+                  onCheckedChange={() => setEmptyDays(!emptyDays)}
                 />
               </div>
-              <div className="ml-3 text-sm">
-                <label htmlFor="show-empty-days" className="font-medium">
-                  Show Empty Days
-                </label>
+              <div className="ml-3">
+                <Label htmlFor="show-empty-days">Show Empty Days</Label>
               </div>
             </div>
 
             <div className="relative mb-4 flex items-start">
-              <div className="flex h-5 items-center">
-                <input
+              <div className="flex items-center">
+                <Checkbox
                   id="show-english-title"
                   aria-describedby="show-english-title-description"
                   name="show-english-title"
-                  type="checkbox"
-                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
                   checked={showEngTitle}
-                  onChange={() => setShowEngTitle(!showEngTitle)}
+                  onCheckedChange={() => setShowEngTitle(!showEngTitle)}
                 />
               </div>
-              <div className="ml-3 text-sm">
-                <label htmlFor="show-english-title" className="font-medium">
-                  Show English Title
-                </label>
+              <div className="ml-3">
+                <Label htmlFor="show-english-title">Show English Title</Label>
               </div>
             </div>
 
             <div className="relative flex items-start">
-              <div className="flex h-5 items-center">
-                <input
+              <div className="flex items-center">
+                <Checkbox
                   id="show-progress"
                   aria-describedby="show-progress-description"
                   name="show-progress"
-                  type="checkbox"
-                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
                   checked={showProg}
-                  onChange={() => setShowProg(!showProg)}
+                  onCheckedChange={() => setShowProg(!showProg)}
                 />
               </div>
-              <div className="ml-3 text-sm">
-                <label htmlFor="show-progress" className="font-medium">
-                  Show Progress
-                </label>
+              <div className="ml-3">
+                <Label htmlFor="show-progress">Show Progress</Label>
               </div>
             </div>
           </div>
