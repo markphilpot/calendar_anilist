d48b92a3ad046d882a9805c0a5279ecfa7f777b5
Mark Philpot <mark.philpot@gmail.com>
2026-07-06 21:33:00 -0700

    refactor: move day border/rounding/layout ownership to Week container

--- src/components/Week.tsx
+++ src/components/Week.tsx
@@ -1,6 +1,7 @@
 import React, { useMemo } from 'react';
 
 import { toPairs } from 'ramda';
+import classNames from 'classnames';
 import Day from './Day';
 import useSettings from '../hooks/useSettings';
 import { AiringScheduleMedia } from '../types';
@@ -24,10 +25,18 @@ const Week = (props: Props) => {
   const scaleCards = pairs.length < 7;
 
   return (
-    <div className={'box-border flex w-full shrink-0 grow basis-auto flex-row flex-wrap p-2 xs:flex-nowrap'}>
-      {pairs.map(([key, list]) => {
-        return <Day key={key} day={key} cards={list} progress={progress} scaleCards={scaleCards} />;
-      })}
+    <div className="box-border w-full shrink-0 grow basis-auto p-2">
+      <div
+        className={classNames(
+          'flex w-full flex-col overflow-hidden rounded border border-black divide-y divide-black',
+          'dark:border-zinc-400 dark:divide-zinc-400',
+          'sm:flex-row sm:divide-x sm:divide-y-0'
+        )}
+      >
+        {pairs.map(([key, list]) => {
+          return <Day key={key} day={key} cards={list} progress={progress} scaleCards={scaleCards} />;
+        })}
+      </div>
     </div>
   );
 };
